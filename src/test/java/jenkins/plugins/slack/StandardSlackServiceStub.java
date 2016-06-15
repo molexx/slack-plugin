212683a137bf7d248774bc654e10d49ac2a669e0
Jingkei Ly <jily@ebay.com>
2016-06-15 14:58:09 +0100

    changed so that the file upload will occur if the message is posted (not only when the build was successful)

--- src/test/java/jenkins/plugins/slack/StandardSlackServiceStub.java
+++ src/test/java/jenkins/plugins/slack/StandardSlackServiceStub.java
@@ -5,7 +5,7 @@ public class StandardSlackServiceStub extends StandardSlackService {
     private HttpClientStub httpClientStub;
 
     public StandardSlackServiceStub(String teamDomain, String token, String roomId) {
-        super(teamDomain, token, roomId);
+        super(teamDomain, token, roomId, null);
     }
 
     @Override
